8ff9d80cccd815c8d93f04645ff4d1db0f42e950
Alex Vernacchia <alvernacchia@gmail.com>
2018-06-10 18:01:20 +0100

    chore: modernize lib

--- test/sample-server.js
+++ test/sample-server.js
@@ -1,44 +1,44 @@
 'use strict';
 
-var express = require( 'express' );
+const express = require('express');
 
-var lessCompilerNoConifg     = require('../lib/less-middleware')();
-var lessCompilerStringConfig = require('../lib/less-middleware')( './public' );
-var lessCompilerObjectConfig = require('../lib/less-middleware')({ publicDir: './public', paths: [ './test' ] });
+const lessCompilerNoConifg = require('../lib/less-middleware')();
+const lessCompilerStringConfig = require('../lib/less-middleware')('./public');
+const lessCompilerObjectConfig = require('../lib/less-middleware')({ publicDir: './public', paths: ['./test'] });
 
-var appNoConfig     = express();
-var appStringConfig = express();
-var appObjectConfig = express();
+const appNoConfig = express();
+const appStringConfig = express();
+const appObjectConfig = express();
 
-appNoConfig.use( lessCompilerNoConifg );
-appStringConfig.use( lessCompilerStringConfig );
-appObjectConfig.use( lessCompilerObjectConfig );
+appNoConfig.use(lessCompilerNoConifg);
+appStringConfig.use(lessCompilerStringConfig);
+appObjectConfig.use(lessCompilerObjectConfig);
 
 // string config
-appNoConfig.get( '/css-files/sample-less.css' );
+appNoConfig.get('/css-files/sample-less.css');
 
-appNoConfig.get( '/css-files/sample.css', function( req, res ) {
+appNoConfig.get('/css-files/sample.css', (req, res) => {
 	// we got here, so we know the compiler was bypassed
-	res.setHeader( 'Content-Type', 'text/css; charset=utf-8' );
-	res.status( 200 ).end();
+	res.setHeader('Content-Type', 'text/css; charset=utf-8');
+	res.status(200).end();
 });
 
-appNoConfig.get( '/css-files/less-does-not-exist.css', function( req, res ) {
-	res.status( 404 ).end();
+appNoConfig.get('/css-files/less-does-not-exist.css', (req, res) => {
+	res.status(404).end();
 });
 
-appNoConfig.post( '/css-files/sample-less.css', function( req, res ) {
-	res.status( 200 ).end();
+appNoConfig.post('/css-files/sample-less.css', (req, res) => {
+	res.status(200).end();
 });
 
-appNoConfig.get( '/css-files/parse-error' );
+appNoConfig.get('/css-files/parse-error');
 
 // object + string configs
-appStringConfig.get( '/css-files/sample-less.css' );
-appObjectConfig.get( '/css-files/sample-less.css' );
+appStringConfig.get('/css-files/sample-less.css');
+appObjectConfig.get('/css-files/sample-less.css');
 
 module.exports = {
-	configNone: appNoConfig
-	, configString: appStringConfig
-	, configObject: appObjectConfig
+	configNone: appNoConfig,
+	configString: appStringConfig,
+	configObject: appObjectConfig
 };
--- test/spec.js
+++ test/spec.js
@@ -1,96 +1,93 @@
 'use strict';
 
-var request = require( 'supertest' );
-var assert  = require( 'assert' );
+const request = require('supertest');
+const assert = require('assert');
 
-var app             = require( './sample-server' ).configNone;
-var appStringConfig = require( './sample-server' ).configString;
-var appObjectConfig = require( './sample-server' ).configObject;
+const app = require('./sample-server').configNone;
+const appStringConfig = require('./sample-server').configString;
+const appObjectConfig = require('./sample-server').configObject;
 
-var assertCompiled = function(done) {
-	return function(err, res) {
-		if( err ) {
-			done( err );
+const compressResult = result => {
+	return result.replace(/\n/g, '').replace(/\s{2,}/g, ' ');
+};
+
+const assertCompiled = done => {
+	return (err, res) => {
+		if (err) {
+			done(err);
 		}
 
-		var result = compressResult( res.text );
+		var result = compressResult(res.text);
 
-		assert.equal( result, '.test .test-inside { color: white;}' );
+		assert.equal(result, '.test .test-inside { color: white;}');
 		done();
 	};
 };
 
-
-describe( 'Express LESS Middleware', function() {
-
-	it( 'should deliver compiled LESS file', function( done ) {
-		request( app )
-			.get( '/css-files/sample-less.css' )
-			.expect( 'Content-Type', 'text/css; charset=utf-8' )
-			.expect( 200 )
+describe('Express LESS Middleware', () => {
+	it('should deliver compiled LESS file', done => {
+		request(app)
+			.get('/css-files/sample-less.css')
+			.expect('Content-Type', 'text/css; charset=utf-8')
+			.expect(200)
 			.end(assertCompiled(done));
 	});
 
-	it( 'should deliver compiled LESS file when configured with string as options', function( done ) {
-		request( appStringConfig )
-			.get( '/css-files/sample-less.css' )
-			.expect( 'Content-Type', 'text/css; charset=utf-8' )
-			.expect( 200 )
+	it('should deliver compiled LESS file when configured with string as options', done => {
+		request(appStringConfig)
+			.get('/css-files/sample-less.css')
+			.expect('Content-Type', 'text/css; charset=utf-8')
+			.expect(200)
 			.end(assertCompiled(done));
 	});
 
-	it( 'should deliver compiled LESS file when configured with object as options', function( done ) {
-		request( appObjectConfig )
-			.get( '/css-files/sample-less.css' )
-			.expect( 'Content-Type', 'text/css; charset=utf-8' )
-			.expect( 200 )
+	it('should deliver compiled LESS file when configured with object as options', done => {
+		request(appObjectConfig)
+			.get('/css-files/sample-less.css')
+			.expect('Content-Type', 'text/css; charset=utf-8')
+			.expect(200)
 			.end(assertCompiled(done));
 	});
 
-	it( 'should deliver parse error', function( done ) {
-		request( app )
-			.get( '/css-files/parse-error.css' )
-			.end( function( err, res ) {
-				if( err ) {
-					done( err );
+	it('should deliver parse error', done => {
+		request(app)
+			.get('/css-files/parse-error.css')
+			.end((err, res) => {
+				if (err) {
+					done(err);
 				}
 
-				var expectedResult = !!res.text.match( /^express\-less\-middleware\:/ );
+				var expectedResult = !!res.text.match(/^express\-less\-middleware\:/);
 
-				assert( expectedResult, true );
+				assert(expectedResult, true);
 				done();
 			});
 	});
 
-	it( 'should not deliver compiled LESS on POST requests', function( done ) {
-		request( app )
-			.post( '/css-files/sample-less.css' )
-			.expect( 200 )
-			.end( function( err, res ) {
-				if( err ) {
-					done( err );
+	it('should not deliver compiled LESS on POST requests', done => {
+		request(app)
+			.post('/css-files/sample-less.css')
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					done(err);
 				}
 
-				assert.equal( res.text, '' );
+				assert.equal(res.text, '');
 				done();
 			});
 	});
 
-	it( 'should not do anything on requests for CSS files', function( done ) {
-		request( app )
-			.get( '/css-files/sample.css' )
-			.expect( 'Content-Type', 'text/css; charset=utf-8' )
-			.expect( 200, done );
+	it('should not do anything on requests for CSS files', done => {
+		request(app)
+			.get('/css-files/sample.css')
+			.expect('Content-Type', 'text/css; charset=utf-8')
+			.expect(200, done);
 	});
 
-	it( 'should not try to read LESS file if CSS file and LESS file do not exist', function( done ) {
-		request( app )
-			.get( '/css-files/less-does-not-exist.css' )
-			.expect( 404, done );
+	it('should not try to read LESS file if CSS file and LESS file do not exist', done => {
+		request(app)
+			.get('/css-files/less-does-not-exist.css')
+			.expect(404, done);
 	});
 });
-
-
-function compressResult( result ) {
-	return result.replace( /\n/g, '' ).replace( /\s{2,}/g, ' ' );
-}
