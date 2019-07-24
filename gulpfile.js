d72b90464f4306e41730318723a6c69a9a0f9c72
Alex Vernacchia <alvernacchia@gmail.com>
2019-07-24 17:50:41 +0100

    remove linting via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,17 @@
 "use strict";
 
 var gulp = require("gulp");
-var jshint = require("gulp-jshint");
 var mocha = require("gulp-mocha");
 
 var paths = {
-  lib: "./lib/**/*.js",
-  gulp: "./gulpfile.js",
-  testSpec: "./test/spec.js",
-  tests: "./test/**/*.js"
+  testSpec: "./test/spec.js"
 };
 
 gulp.task("default", ["test"]);
-gulp.task("test", ["jshint", "mocha"]);
-
-gulp.task("jshint", function() {
-  return gulp
-    .src([paths.lib, paths.gulp, paths.tests])
-    .pipe(jshint())
-    .pipe(jshint.reporter("jshint-stylish"))
-    .pipe(jshint.reporter("fail"));
-});
+gulp.task("test", ["mocha"]);
 
 // only doing this at the end so the logs don't get messed up by other tasks going
-gulp.task("mocha", ["jshint"], function() {
+gulp.task("mocha", function() {
   return gulp.src(paths.testSpec, { read: false }).pipe(
     mocha({
       reporter: "spec"
